b0af141d49614459267098f8d3014cd26ca10cd5
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2022-10-12 17:50:09 +0300

    added c5-04

--- contest5/c5_04.c
+++ contest5/c5_04.c
@@ -7,120 +7,56 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <string.h>
-#include <fcntl.h>
 #include <limits.h>
+#include <fcntl.h>
 
 struct File
 {
-    int fd;
     char *path;
+    ino_t fd;
 };
 
-
-typedef struct Node
-{
-    struct Node *l, *r;
-    struct File f;
-} Node;
-
-
 int
-set_reg_file(const char *path, struct stat *stb)
+cmp(const void *a, const void *b)
 {
-    if (S_ISREG(stb->st_mode)) {
-        return 0;
-    } else {
-        char buf[PATH_MAX + 1];
-        if (readlink(path, buf, PATH_MAX + 1) == -1 || lstat(buf, stb)) {
-            return -1;
-        } else {
-            return set_reg_file(buf, stb);
-        }
-    }
+    return strcmp((*(struct File *) a).path, (*(struct File *) b).path);
 }
 
-int
-same_file(const char *path1, const char *path2)
-{
-    struct stat stb1, stb2;
-    if (lstat(path1, &stb1) == -1 || lstat(path2, &stb2) == -1) {
-        return -1;
-    } else if (S_ISREG(stb1.st_mode) && S_ISREG(stb2.st_mode)) {
-        return stb1.st_ino == stb2.st_ino;
-    } else if (S_ISREG(stb1.st_mode) && S_ISLNK(stb2.st_mode)) {
-        if (set_reg_file(path1, &stb1) == -1) {
-            return -1;
-        } else {
-            return stb1.st_ino == stb2.st_ino;
-        }
-    } else if (S_ISLNK(stb1.st_mode) && S_ISREG(stb2.st_mode)) {
-        if (set_reg_file(path2, &stb2) == -1) {
-            return -1;
-        } else {
-            return stb1.st_ino == stb2.st_ino;
-        }
-    } else if (S_ISLNK(stb1.st_mode) && S_ISLNK(stb2.st_mode)) {
-        if (set_reg_file(path1, &stb1) == -1 || set_reg_file(path2, &stb2) == -1) {
-            return -1;
-        } else {
-            return stb1.st_ino == stb2.st_ino;
-        }
-    }
-    return 0;
-}
-
-void
-add(Node *this, char *path)
-{
-    if (same_file(path, this->f.path) == 1) {
-        if (strcmp(path, this->f.path) > 0) {
-            this->f.path = path;
-            this->f.fd = open(path, O_RDONLY, 0666);
-        }
-    } else if (strcmp(path, this->f.path) > 0) {
-        if (this->r == NULL) {
-            add(this->r, path);
-        } else {
-            this->r = calloc(1, sizeof(Node));
-            this->r->f = (struct File) { .fd = open(path, O_RDONLY, 0666), .path = path };
-            this->r->r = NULL;
-            this->r->l = NULL;
-        }
-    } else {
-        if (this->l == NULL) {
-            this->l = calloc(1, sizeof(Node));
-            this->l->f = (struct File) { .fd = open(path, O_RDONLY, 0666), .path = path };
-            this->l->r = NULL;
-            this->l->l = NULL;
-        } else {
-            add(this->l, path);
-        }
-    }
-}
-
-void
-print(Node *this)
-{
-    if (this->l != NULL) {
-        print(this->l);
-    }
-    printf("%s\n", this->f.path);
-    if (this->r != NULL) {
-        print(this->r);
-    }
-}
 
 int
 main(int argc, char *argv[])
 {
-    if (argc <= 1) {
+    if (argc == 1) {
         return EXIT_SUCCESS;
     } else {
-        Node nd = (Node) { .l = NULL, .r = NULL, .f = (struct File) { .path = argv[1], .fd = open(argv[1], O_RDONLY,
-                                                                                                  0666) }};
-        for (int i = 2; i < argc; ++i) {
-            add(&nd, argv[i]);
+        int sz = 0;
+        struct File *arr = calloc(argc - 1, sizeof(struct File));
+        for (int i = 1; i < argc; ++i) {
+            struct stat stb;
+            if (stat(argv[i], &stb) == -1) {
+                continue;
+            }
+            int flag = 0;
+            for (int j = 0; j < sz; ++j) {
+                struct stat tmp;
+                stat(arr[j].path, &tmp);
+                if (stb.st_ino == tmp.st_ino) {
+                    if (strcmp(argv[i], arr[j].path) > 0) {
+                        arr[j] = (struct File) { .path = argv[i], .fd = stb.st_ino };
+                    }
+                    flag = 1;
+                    break;
+                }
+            }
+            if (!flag) {
+                arr[sz] = (struct File) { .path = argv[i], .fd = stb.st_ino };
+                ++sz;
+            }
         }
-        print(&nd);
+        qsort(arr, sz, sizeof(struct File), cmp);
+        for (int i = 0; i < sz; ++i) {
+            printf("%s\n", arr[i].path);
+        }
+        return EXIT_SUCCESS;
     }
 }
